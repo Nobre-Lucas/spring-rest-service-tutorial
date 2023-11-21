b16f64b8fe013a5284bac880cdac99406c94f20a
Nobre-Lucas <lucasbnobre18@gmail.com>
2023-11-21 10:22:09 -0300

    First Commit

---
+++ src/main/java/com/example/gsrestservice/GsrestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsrestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsrestserviceApplication.class, args);
+	}
+
+}
